c2af66260d27b240944d3b2a7e45d4afbf30713a
robustjumprope <91493312+robustjumprope@users.noreply.github.com>
2022-01-04 17:51:58 -0500

    Added username and project fields and set up database

--- notes-app/scripts/firebase.js
+++ notes-app/scripts/firebase.js
@@ -1,20 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
+import { getFirestore, collection } from "firebase/firestore"
 import svelte from 'svelte'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD80QzkFZec5R05eXzOPnViDVTc0Mazcwk",
-  authDomain: "notes-app-eacae.firebaseapp.com",
-  projectId: "notes-app-eacae",
-  storageBucket: "notes-app-eacae.appspot.com",
-  messagingSenderId: "590624021476",
-  appId: "1:590624021476:web:b7bc8b881884962e43c5f1"
+  apiKey: "AIzaSyAhrDn1CC-g77Do1F4Wk3k27fZbRQT4e-0",
+  authDomain: "notes-app-eb0d6.firebaseapp.com",
+  projectId: "notes-app-eb0d6",
+  storageBucket: "notes-app-eb0d6.appspot.com",
+  messagingSenderId: "1018974169633",
+  appId: "1:1018974169633:web:dba0e9d35d8e0775c09160"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
+const colRef = collection(db, "notes");
+
--- notes-app/src/components/Editor.svelte
+++ notes-app/src/components/Editor.svelte
@@ -3,24 +3,30 @@
     import { collection, addDoc } from "firebase/firestore"; 
     import { db } from '../../scripts/firebase';
     let text = 'Start typing your note!';
+    let username;
+    let project;
 
     async function addToDb (text) {
         let note = text;
         let date = new Date().toDateString();
         const doc = await addDoc(collection(db, 'notes'), {
             createdAt: date,
-            note: note
+            note: note,
+            user: username,
+            project: project
         });
-        console.log(doc)
+        console.log(doc.data)
     }
 </script>
 
 <div class="editor">
+    <label>User:<input bind:value={username} type="text" placeholder="Please enter your username"></label>
+    <label>Project:<input bind:value={project} type="text" placeholder="Please enter your project name"></label>
     <textarea class="note-input" bind:value={text}></textarea>
     <div class="note-output">
         {@html marked(text)}
     </div>
-    <button on:click|preventDefault={addToDb(marked(text))} class="btn-submit">Add Note</button>
+    <button on:click|preventDefault={addToDb(text)} class="btn-submit">Add Note</button>
 </div>
 
 <style>
